18941edbd788a30f55f2ec021d0c091b92c99880
liamcolangelo <106918396+liamcolangelo@users.noreply.github.com>
2022-11-26 14:18:41 -0500

    Bug fixes in update.py and updateToTesting.py

--- updateToTesting.py
+++ updateToTesting.py
@@ -3,7 +3,7 @@ import os
 print("Downloading updated repository as temporary files")
 os.system("git clone --single-branch --branch testing https://github.com/liamcolangelo/BASIC.git")
 print("Removing old files")
-os.system("sudo rm -r -f -a /usr/bin/basic/.")
+os.system("sudo rm -r -f /usr/bin/basic/.")
 print("Copying new files")
 os.system("sudo cp -a tempBASIC/. /usr/bin/basic/")
 print("Removing temporary files")
