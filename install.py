2fa7d5ec893a900319933ce7e148617504928327
liamcolangelo <106918396+liamcolangelo@users.noreply.github.com>
2022-11-26 18:19:01 -0500

    Splits runBasic.py into runBasic.py and default.py for improved readabiliy and eventual expansion. install.py has also been modified.

--- install.py
+++ install.py
@@ -2,20 +2,8 @@ import os
 
 print("Making directory /usr/bin/basic")
 os.system("sudo mkdir /usr/bin/basic")
-print("Making file install.py")
-os.system("sudo cp install.py /usr/bin/basic/install.py")
-print("Making file runBasic.py")
-os.system("sudo cp runBasic.py /usr/bin/basic/runBasic.py")
-print("Making file README.md")
-os.system("sudo cp README.md /usr/bin/basic/README.md")
-print("Making file GUESSANUMBER.BAS")
-os.system("sudo cp GUESSANUMBER.BAS /usr/bin/basic/GUESSANUMBER.BAS")
-print("Making file uninstall.py")
-os.system("sudo cp uninstall.py /usr/bin/basic/uninstall.py")
-print("Making file updateToTesting.py")
-os.system("sudo cp updateToTesting.py /usr/bin/basic/updateToTesting.py")
-print("Making file update.py")
-os.system("sudo cp update.py /usr/bin/basic/update.py")
+print("Installing files")
+os.system("sudo cp ./* /usr/bin/basic")
 print("Making alias basic=python3 /usr/bin/basic/runBasic.py")
 with open(os.path.join(os.path.expanduser('~'), '.bashrc'), 'r') as f:
     file = f.read() + "\n" + "alias basic='python3 /usr/bin/basic/runBasic.py'"
