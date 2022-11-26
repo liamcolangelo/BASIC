f90a4c791bf0060629270fda730a031bbbea3442
liamcolangelo <106918396+liamcolangelo@users.noreply.github.com>
2022-11-26 14:21:32 -0500

    Bug fix in uninstall.py

--- uninstall.py
+++ uninstall.py
@@ -2,23 +2,8 @@ import os
 
 sure = input("Are you sure you would like to uninstall the BASIC interpreter y/n : ")
 if sure.lower() == "y":
-    print("Uninstalling GUESSANUMBER.BAS")
-    os.system("sudo rm /usr/bin/basic/GUESSANUMBER.BAS")
-    print("Uninstalling install.py")
-    os.system("sudo rm /usr/bin/basic/install.py")
-    print("Uninstalling README.md")
-    os.system("sudo rm /usr/bin/basic/README.md")
-    print("Uninstalling runBasic.md")
-    os.system("sudo rm /usr/bin/basic/runBasic.py")
-    print("Uninstalling uninstall.py")
-    os.system("sudo rm /usr/bin/basic/uninstall.py")
-    print("Uninstalling update.py")
-    os.system("sudo rm /usr/bin/basic/update.py")
-    print("Uninstalling updateToTesting.py")
-    os.system("sudo rm /usr/bin/basic/updateToTesting.py")
-    print("Removing basic directory")
-    os.system("sudo rmdir /usr/bin/basic")
-    
+    print("Uninstalling files")
+    os.system("sudo rm -r -f /usr/bin/basic")
     print("Removing basic alias")
     with open(os.path.join(os.path.expanduser('~'), '.bashrc'), 'r') as f:
         file = f.read()
