70c4d721f4e64a0d6ee8e0c26446abc97c52a8a8
liamcolangelo <106918396+liamcolangelo@users.noreply.github.com>
2022-11-26 11:13:11 -0500

    Fix update.py

--- update.py
+++ update.py
@@ -3,7 +3,7 @@ import os
 print("Downloading updated repository as temporary files")
 os.system("git clone https://github.com/liamcolangelo/BASIC.git tempBASIC")
 print("Copying new files")
-os.system("sudo cp -a temp/BASIC/. /user/bin/basic/")
+os.system("sudo cp -a tempBASIC/. /user/bin/basic/")
 print("Removing temporary files")
 os.system("cd ..")
 os.system("rm -r -f tempBASIC")
